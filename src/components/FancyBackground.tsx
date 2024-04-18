5466209caa6319505a918d8fa729825b53099e45
Peter Morrison <bwt.bluewebtech@gmail.com>
2024-04-17 22:22:06 -0400

    Cleaned up code and split out navigation into component.

---
+++ src/components/FancyBackground.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import LocalStorage from '../utilities/LocalStorage.ts';
+import DisneyMovies from '../data/disney-movies.json';
+
+interface FancyBackgroundItem {
+  color: string;
+  text: string;
+}
+
+export default function FancyBackground() {
+  const [fancyBackgroundItems, setFancyBackgroundItems] = useState<FancyBackgroundItem[]>([]);
+
+  const isRoot = window.location.pathname === '/';
+
+  // I stole this code from the stackoverflow.
+  // Thanks Internet developer person!
+  const randomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b}, 0.3)`;
+  };
+
+  useEffect(() => {
+    if (LocalStorage.has('fancy-background')) {
+      const fancyBackground = JSON.parse(LocalStorage.get('fancy-background'));
+      setFancyBackgroundItems((fancyBackgroundItems) => fancyBackground);
+    } else {
+      const fancyBackgroundToString = DisneyMovies.join(', ').replace(/[^a-zA-Z ]/g, '');
+      const fancyBackgroundCollection: FancyBackgroundItem[] = [];
+      const fancyBackgroundBase64 = btoa(fancyBackgroundToString);
+
+      if (fancyBackgroundBase64) {
+        for (let i = 0; i < fancyBackgroundBase64.length; i++) {
+          fancyBackgroundCollection.push({
+            color: randomColor(),
+            text: fancyBackgroundBase64[i],
+          });
+        }
+
+        setFancyBackgroundItems((fancyBackgroundItems) => fancyBackgroundCollection);
+
+        const fancyBackground = JSON.stringify(fancyBackgroundCollection);
+        LocalStorage.set('fancy-background', fancyBackground);
+      }
+    }
+  }, []);
+
+  const backgroundChars = fancyBackgroundItems.map((item, key) => {
+    return (<span className="mr-1 inline-flex" style={{color: item.color}} key={key}>{item.text}</span>)
+  });
+
+  return (<div className="fixed m-0 p-0 select-none">{isRoot ? backgroundChars : ''}</div>);
+};
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+  MdOutlineHome,
+  MdOutlinePerson,
+  // MdOutlineMusicNote,
+  // MdOutlineSettings,
+  MdOutlineTerminal,
+} from "react-icons/md";
+
+export default function Navigation() {
+  const [firstFade, setFirstFade] = useState<boolean>(false);
+
+  const doFadeIn = () => setTimeout(() => setFirstFade(true), 1);
+
+  doFadeIn();
+
+  return (
+    <nav className={`${ firstFade ? "transition-all duration-500 opacity-100" : "opacity-0"}`}>
+      <ul className="flex text-white font-light">
+        <li className="mr-5 px-1 py-1">
+          <a href="/">
+            <MdOutlineHome className="mr-2 hidden sm:inline text-xl text-blue-500 relative bottom-0.5" />
+            <div className="inline-block transition duration-200 ease-in-out hover:text-blue-300">Home</div>
+          </a>
+        </li>
+        <li className="mr-5 px-1 py-1">
+          <a href="/about">
+            <MdOutlinePerson className="mr-2 hidden sm:inline text-xl text-purple-500 relative bottom-0.5" />
+            <div className="inline-block transition duration-200 ease-in-out hover:text-purple-300">About</div>
+          </a>
+        </li>
+        {/* <li className="mr-5 px-1 py-1">
+          <a href="/music">
+            <MdOutlineMusicNote className="mr-2 hidden sm:inline text-xl text-pink-500 relative bottom-0.5" />
+            <div className="inline-block transition duration-200 ease-in-out hover:text-pink-300">Music</div>
+          </a>
+        </li>
+        <li className="mr-5 px-1 py-1">
+          <a href="/settings">
+            <MdOutlineSettings className="mr-2 hidden sm:inline text-xl text-orange-500 relative bottom-0.5" />
+            <div className="inline-block transition duration-200 ease-in-out hover:text-orange-300">Settings</div>
+          </a>
+        </li> */}
+        <li className="mr-5 px-1 py-1">
+          <a href="/terminal">
+            <MdOutlineTerminal className="mr-2 hidden sm:inline text-xl text-gray-500 relative bottom-0.5" />
+            <div className="inline-block transition duration-200 ease-in-out hover:text-gray-300">Terminal</div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
